555dd104d53fa9f7fc7522145b3cb9960679b9d4
Pablo <pmotta.dev@gmail.com>
2019-10-04 13:42:13 -0700

    data from edamam

---
+++ js/pabloNotes.js
@@ -0,0 +1,6 @@
+// edamam
+response.hits[0 - 4].recipe.lable; //
+response.hits[0 - 4].recipe.image;
+response.hits[0 - 4].recipe.url;
+response.hits[0 - 4].recipe.healthLabels; // array
+response.hits[0 - 4].recipe.dietLabels; // array
