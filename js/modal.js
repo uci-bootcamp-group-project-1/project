fb1dfb5b56c0390b8d42dfbbf2dee9f099b4a7cf
cloud xu <xuanlinxu1004@gmail.com>
2019-10-04 13:34:14 -0700

    modified modal position and size, adding js for modal layout @ 13:34:14on10/04/19

---
+++ js/modal.js
@@ -0,0 +1,58 @@
+// layout for modal
+var $modal = $(".api-data");
+$("<div>", {
+    class: "row"
+})
+    .append(
+        $("<div>", {
+            class: "col-lg-6 col-md-12 mx-auto",
+            css: {
+                "margin-bottom": "10px"
+            }
+        }).append(
+            $("<div>", {
+                class: "center-wrap",
+                css: {
+                    margin: "0 auto",
+                    border: "1px solid lightgrey"
+                }
+            }).append(
+                $("<h4>", {
+                    class: "data-title",
+                    html: "Receipts"
+                }),
+                $("<div>", {
+                    class: "rp-cards"
+                })
+            )
+        ),
+        $("<div>", {
+            class: "col-lg-6 col-md-12 mx-auto",
+            css: {
+                "margin-bottom": "10px"
+            }
+        }).append(
+            $("<div>", {
+                class: "center-wrap",
+                css: {
+                    margin: "0 auto",
+                    border: "1px solid lightgrey"
+                }
+            }).append(
+                $("<h4>", {
+                    class: "data-title",
+                    html: "Yelp Search"
+                }),
+                $("<ul>", {
+                    class: "yp-cards"
+                })
+            )
+        )
+    )
+    .appendTo($modal);
+
+var yelpdb = {
+    "rp-cards": {
+        rc1: {}
+    }
+};
