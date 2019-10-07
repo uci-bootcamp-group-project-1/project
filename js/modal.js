05c0e02f2184e22859fadb0ceea97a6866f34379
cloud xu <xuanlinxu1004@gmail.com>
2019-10-07 14:30:07 -0700

    save

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,12 @@
 // layout for modal
 var $modal = $(".api-data");
 $("<div>", {
-    class: "row"
+    class: "row theMatrix",
+    "data-status": "none"
 })
     .append(
-<<<<<<< HEAD
         $("<div>", {
             class: "col-lg-6 col-md-12 mx-auto",
-=======
-        $('<div>', {
-            class: 'col-md-6 col-sm-12 mx-auto',
->>>>>>> master
             css: {
                 "margin-bottom": "10px"
             }
@@ -18,38 +14,20 @@ $("<div>", {
             $("<div>", {
                 class: "center-wrap",
                 css: {
-<<<<<<< HEAD
-                    margin: "0 auto",
-                    border: "1px solid lightgrey"
+                    margin: "0 auto"
                 }
             }).append(
                 $("<h4>", {
-                    class: "data-title",
+                    class: "data-title mb-5 text-uppercase",
                     html: "Recipes"
                 }),
                 $("<div>", {
-                    class: "rp-cards"
+                    class: "recipe-cards"
                 })
             )
         ),
         $("<div>", {
             class: "col-lg-6 col-md-12 mx-auto",
-=======
-                    margin: '0 auto'
-                }
-            }).append(
-                $('<h4>', {
-                    class: 'data-title mb-5',
-                    html: 'Recipes'
-                }),
-                $('<div>', {
-                    class: 'recipe-cards'
-                })
-            )
-        ),
-        $('<div>', {
-            class: 'col-md-6 col-sm-12 mx-auto',
->>>>>>> master
             css: {
                 "margin-bottom": "10px"
             }
@@ -57,86 +35,17 @@ $("<div>", {
             $("<div>", {
                 class: "center-wrap",
                 css: {
-<<<<<<< HEAD
-                    margin: "0 auto",
-                    border: "1px solid lightgrey"
+                    margin: "0 auto"
                 }
             }).append(
                 $("<h4>", {
-                    class: "data-title",
+                    class: "data-title mb-5 text-uppercase",
                     html: "Find Your Food Nearby"
                 }),
-                $("<ul>", {
-                    class: "yp-cards"
-=======
-                    margin: '0 auto'
-                }
-            }).append(
-                $('<h4>', {
-                    class: 'data-title mb-5',
-                    html: 'Find Your Food Nearby'
-                }),
-                $('<div>', {
-                    class: 'yelp-cards',
-                    html: 'yelp cards'
->>>>>>> master
+                $("<div>", {
+                    id: "yelp-cards"
                 })
             )
         )
     )
     .appendTo($modal);
-<<<<<<< HEAD
-
-var $reci = $(".rp-cards");
-var $yelp = $(".yp-cards");
-
-for (var i = 0; i < 5; i++) {
-    $("<div>", {
-        class: "card mb-3 card" + i,
-        css: { "max-width": "540px" }
-    })
-        .append(
-            $("<div>", {
-                class: "row no-gutters"
-            }).append(
-                $("<div>", {
-                    class: "col-md-4"
-                }).append(
-                    $("<img>", {
-                        class: "card-img image_url"
-                    })
-                ),
-                $("<div>", {
-                    class: "col-md-8"
-                }).append(
-                    $("<div>", {
-                        class: "card-body"
-                    }).append(
-                        $("<h5>", {
-                            class: "card-title name"
-                        }),
-                        $("<div>", {
-                            class: "rating",
-                            css: { display: "inline-block" }
-                        }),
-                        $("<p>", {
-                            class: "review_count"
-                        }),
-                        $("<hr>"),
-                        $(
-                            "<div>",
-                            {
-                                class: "location"
-                            },
-                            $("<p>", {
-                                class: "cateogories"
-                            })
-                        )
-                    )
-                )
-            )
-        )
-        .appendTo($yelp);
-}
-=======
->>>>>>> master
